d67c44120b18c61088937df30031e974d310be2f
anas1993 <shaikhanas1993@gmail.com>
2019-06-05 10:42:31 +0530

    dummy local api experiment on linux

---
+++ Models/DummyContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace crudApi.Models
+{
+    public partial class DummyContext : DbContext
+    {
+
+         public IConfiguration Configuration { get; }
+        public DummyContext()
+        {
+        }
+
+        public DummyContext(DbContextOptions<DummyContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+        }
+    }
+}
---
+++ Repositories/Dao.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using crudApi.Models;
+
+namespace crudApi.Repositories
+{
+   public interface IDao<TEntity,U> where TEntity : class
+    {
+        IEnumerable<TEntity> GetUsers();
+  //      TEntity GetUser(U id);
+   //     int AddUser(TEntity b);
+    //    int UpdateUser(U id,TEntity b);
+     //   int DeleteUser(U id); 
+    }
+
+    public class Dao : IDao<User, int>
+    {
+            DummyContext ctx;
+            public Dao(DummyContext c)
+            {
+                        ctx = c;
+            }
+
+            public IEnumerable<User> GetUsers()
+            {
+                        var users = ctx.User.ToList();
+                        return users;
+            }
+    }
+
+
+
+}
